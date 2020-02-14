0065ae6c85c54f97f79f95c2a31ca84f2e3eb76c
Gusabary <gusabary@126.com>
2020-02-14 02:56:32 +0000

    feat: add Utils class

---
+++ src/app/Utils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdlib.h>
+#include <sys/ptrace.h>
+
+namespace SAIL { namespace util {
+
+class Utils
+{
+public:
+    int readStrFrom(int tid, const char *p, char *buf, size_t s);
+    int readBytesFrom(int tid, const char *p, char *buf, size_t s);
+};
+
+}}
